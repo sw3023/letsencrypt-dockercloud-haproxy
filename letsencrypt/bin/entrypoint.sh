204e7ffba691a117b4bc6fd3bd4a2b5ef8eebd9e
Christian Nadeau <cnadeau@macadamian.com>
2017-01-19 09:41:51 -0500

    Fixes letsencrypt
    - Start the SimpleHTTPServer on container start
    - Use nc to wait until it's up and running
    - Use nc to wait for the load balancer's instance to be up and running

--- letsencrypt/bin/entrypoint.sh
+++ letsencrypt/bin/entrypoint.sh
@@ -5,13 +5,25 @@ set -e
 # Validate required environment variables.
 [[ -z "$DOMAINS" ]] && MISSING="$MISSING DOMAINS"
 [[ -z "$EMAIL" ]] && MISSING="$MISSING EMAIL"
+[[ -z "$LOAD_BALANCER_SERVICE_NAME" ]] && MISSING="$MISSING LOAD_BALANCER_SERVICE_NAME"
+
 if [[ -n "$MISSING" ]]; then
-	echo "Missing required environment variables: $MISSING" >&2
-	exit 1
+  echo "Missing required environment variables: $MISSING" >&2
+  exit 1
 fi
 
+# Create a web server right away, otherwise HAProxy might not see that
+# the server is listening and you won't be able to authenticate
+echo "Creating http server port 80"
+mkdir -p /opt/www
+(pushd /opt/www && python -m SimpleHTTPServer 80) &
+
+# Wait for the local server to be listening
+while ! nc -z localhost 80; do echo "SimpleHTTPServer not up yet, waiting 1 second"; sleep 1; done
+
 # Wait for HAproxy to start before updating certificates on startup.
-# TODO: Use Dockerize, instead of assuming it takes 60 seconds to start.
-(sleep 60; update-certs.sh) &
+while ! nc -z "$LOAD_BALANCER_SERVICE_NAME" 80; do echo "Loadbalancer not up yet, waiting 5 second"; sleep 5; done
+
+(update-certs.sh) &
 
 exec "$@"
--- letsencrypt/bin/update-certs.sh
+++ letsencrypt/bin/update-certs.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-# The certbot standalone plugin returns 503 errors. Perhaps because some time
-# is needed after starting before HAproxy can detect it. So run our own web
-# server and use the webroot plugin.
-if [[ -z "$(ps | grep python | grep -v grep)" ]]; then
-	mkdir -p /opt/www
-	(cd /opt/www && python -m SimpleHTTPServer 80) &
-	sleep 1
-fi
-
 # Certificates are separated by semi-colon (;). Domains on each certificate are
 # separated by comma (,).
 CERTS=(${DOMAINS//;/ })
