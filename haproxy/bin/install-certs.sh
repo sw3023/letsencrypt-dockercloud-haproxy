62aa4286eff98b8f10c482b07de44837c29c82c5
Shuhao <sw3023@columbia.edu>
2019-11-28 01:21:57 -0800

    Update install-certs.sh

--- haproxy/bin/install-certs.sh
+++ haproxy/bin/install-certs.sh
@@ -18,7 +18,7 @@ fi
 if [[ -n "$(ls -A $LIVE_CERT_FOLDER)" ]]; then
 	COUNT=0
 	for DIR in "$LIVE_CERT_FOLDER"/*; do
-	    if [ "foo" = "foo" ]; then
+	    if [ "foo" != "README" ]; then
 		cat "$DIR/privkey.pem" "$DIR/fullchain.pem" > /certs/letsencrypt$COUNT.pem
 		(( COUNT += 1 ))
 	    fi
